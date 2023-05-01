297ba27c07183554009a05f2bb95672e26874790
canattmaca <119670826+canattmaca@users.noreply.github.com>
2023-05-01 16:35:51 +0300

    Homework- 8

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
-const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-app.get('/', (req, res) => {
-  res.send(blog);
-});
+const ejs = require('ejs');
 
+//template engine
+app.set('view engine', 'ejs');
+
+//middleware
+app.use(express.static('public'));
+
+//Routes
+app.get('/', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('add');
+});
+app.get('/post', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('post');
+});
 app.listen(port, () => {
   console.log(`Appjs listening on port ${port}`);
 });
